557033a8528038aaaaf118ac3999ad2f031fda85
mngagan <mngagan@gmail.com>
2020-08-04 18:39:19 +0530

    2 files modified, addes axios for testing

--- components/getQuestions.js
+++ components/getQuestions.js
@@ -1,12 +1,38 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+const axios = require("axios");
 
-class GetQuestions extends Component{
-  render(){
-    return(
-      <div>from get questions</div>
-    )
+class GetQuestions extends Component {
+  handleMock = () => {
+    console.log("in mock service called");
+    axios
+      .get("https://pqt1i0myrj.execute-api.ap-south-1.amazonaws.com/dev/quiz-1/topics")
+      .then(function(response) {
+        // handle succs
+        console.log('success', response.data);
+      })
+      .catch(function(error) {
+        // handle error
+        console.log('error',error);
+      })
+  };
+
+  render() {
+    return (
+      <div>
+        from get questions
+        <hr />
+        <button
+          
+          onClick={() => {
+            this.handleMock();
+          }}
+        >
+          mock service
+        </button>
+      </div>
+    );
   }
 }
 
-export default GetQuestions
+export default GetQuestions;
